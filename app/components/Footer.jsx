1a62565053d19296a312e6964f8c3b2d76ea44a7
yuta-hamasaki <yh02052002@gmail.com>
2024-07-11 16:40:39 -0700

    fixed footer style

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex flex-col w-full b-0 h-auto bg-blue-50 py-11 '>
-      <h2 className='font-bold text-xl flex items-center ml-10 justify-center'>九陽商事</h2>
+      <h2 className='font-bold text-xl flex items-center justify-center'>九陽商事</h2>
       <div className='ml-4 '>
         <ul className='flex flex-row justify-center'>
           <li className='m-2'>会社概要</li>
